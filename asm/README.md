c703b821d0e814f48f675b0d615d9538b4ea421f
Aiden Makovicka <apmak4@gmail.com>
2025-02-05 13:21:48 -0600

    Added Initial ASM folder with TCP Vegas files

---
+++ asm/README.md
@@ -0,0 +1,40 @@
+# ACK-Scalable Method (ASM): Scalably check the multi-ACK properties of Linux TCP congestion control algorithm implementations
+
+
+This repository contains the source code and experiment scripts for paper "Scalable Verification of Multi-ACK Properties in Loss-Based Congestion Control Implementations" published in ICNP 2024. 
+
+## Introduction
+
+Congestion control algorithms, such as RENO and CUBIC, are vital for the Internet. However, numerous bugs have been discovered and reported in the Congestion Control Algorithm Implementations (CCAIs), even in those that have been extensively tested and used on the Internet for years, such as Linux RENO and Linux CUBIC. Some of these bugs have potentially severe impacts on the performance and stability of the Internet. Unfortunately, current CCAI testing and verification methods are inadequate for proving the absence of bugs, require substantial verification expertise, or are not scalable to a large number of acknowledgment packets (ACKs) that trigger CCAI actions. To address all these shortcomings, we propose an ACK Scalable Method, called ASM, and conduct experiments with two representative loss-based CCAIs, Linux RENO and CUBIC.
+
+## Instructions
+
+* Our code and scripts are written and tested on Ubuntu Linux. 
+* Install [KLEE](https://klee-se.org/) to ~/klee_build
+* Download the ASM code to ~/asm
+
+## Experiments
+
+There are three groups of experiments.
+
+### Linux RENO experiments
+
+* The scripts are located in the ~/asm/test_script/aimd directory.
+* `plan_eq.sh` is the script to check the equivalence between the aggregation algorithm and the original RENO.
+* `plan_agg_property.sh` is the script to check the maximum increment properties of RENO using the aggregation algorithm.
+* `plan_ori_property.sh` is the script to check the maximum increment properties of RENO using the original RENO.
+
+### Linux CUBIC experiments
+
+* The scripts are located in the ~/asm/test_script/cubic directory.
+* `plan_eq.sh` is the script to check the equivalence between the aggregation algorithm and the original CUBIC.
+* `plan_agg_property.sh` is the script to check the maximum increment properties of CUBIC using the aggregation algorithm.
+* `plan_ori_property.sh` is the script to check the maximum increment properties of CUBIC using the original CUBIC.
+* `plan_property_target.sh` is the script to check the maximum increment properties of CUBIC in extreme network environments.
+
+### Linux RENO and CUBIC experiments
+
+* The scripts are located in the ~/asm/test_script/friendliness directory.
+* `plan.sh` is the script to check the friendliness property of CUBIC.
+* The Mininet experiment scripts and results are available [here](https://github.com/zmrui/tcp_cubic_fix).
+* We have reported three bugs and patches to Linux maintainers [here](https://lore.kernel.org/netdev/20240810223130.379146-1-mrzhang97@gmail.com/t/#u).
---
+++ asm/asm/test_scripts/vegas/property-maxincrement.c
@@ -0,0 +1,140 @@
+/**
+ * TCP Vegas Original Algorithm Verification
+ * 
+ * This file checks the correctness of the original implementaton of TCP Vegas
+ * so that it can be checked to the aggregate algorithm version. 
+ * 
+ */
+
+#ifndef script_property
+#define script_property
+#include "../../stub_model/header-new/share.h"
+#include "../asm/asm/CA_variants/tcp_vegas.c";
+
+extern struct tcp_congestion_ops tcp_vegas;
+
+u32 __VERIFIER_assert(char *msg, int cond)
+{
+	char printmsg[1024];
+	if (cond){
+		sprintf (printmsg, "\x1B[32m%s passed! \x1B[0m\n", msg);
+		important_msg(printmsg);
+		return 0;
+	} else {
+		sprintf (printmsg, "\x1B[31m%s violated!\x1B[0m\n", msg);
+		important_msg(printmsg);
+		return 1;
+	}
+}
+
+void __VERIFIER_nondet_u32(u32 *variable, char *name, u32 min_value, u32 max_value) 
+{
+#ifdef KLEE
+	klee_make_symbolic (variable, sizeof(u32), name);
+	klee_assume ((*variable) >= min_value);
+	if (klee_is_symbolic(min_value))
+		printf("[Symbolic value] %-25s : min=some symbolic variable\n", name);
+	else
+		printf("[Symbolic value] %-25s : min=%u\n", name, min_value);
+	klee_assume ((*variable) <= max_value);
+	if (klee_is_symbolic(max_value))
+		printf("[Symbolic value] %-25s : max=some symbolic variable\n", name);
+	else
+		printf("[Symbolic value] %-25s : max=%u\n", name, max_value);
+#else
+	(*variable) = max_value;
+	printf("[Concrete value] %s = %u\n", name, max_value);
+#endif
+}
+
+void copy_tcp(struct tcp_sock *tpa, struct vegas *caa, struct tcp_sock *tpb, struct vegas *cab)
+{
+	tpb->snd_cwnd = tpa->snd_cwnd;
+	tpb->snd_ssthresh = tpa->snd_ssthresh;
+	tpb->snd_cwnd_cnt = tpa->snd_cwnd_cnt;
+
+    cab->baseRTT = caa->baseRTT;
+    cab->beg_snd_cwnd = caa->beg_snd_cwnd;
+    cab->beg_snd_una = caa->beg_snd_una;
+    cab->beg_snd_nxt = caa->beg_snd_nxt;
+    cab->doing_vegas_now = caa->doing_vegas_now;
+    cab->cntRTT = caa->cntRTT;
+    cab->minRTT = caa->minRTT;
+
+}
+
+void print_tcp(char *msg, struct tcp_sock *tp, struct vegas *ca){
+    return;
+	//printf("%s: target %u, cwnd %u, K %u, origin %u, last_max %u, snd_cwnd_cnt %u, cnt %u, last_cwnd %u, last_time %u, tcp_cwnd %u, ack_cnt %u\n", msg,
+	//	ca->bic_target, tp->snd_cwnd, ca->bic_K, ca->bic_origin_point, ca->last_max_cwnd, tp->snd_cwnd_cnt, ca->cnt, ca->last_cwnd, ca->last_time, ca->tcp_cwnd, ca->ack_cnt);
+}
+
+
+int main(int argc, char* argv[]){
+
+///////// Parameter initilization ///////////////////////
+
+    // u32 MIN_SSTRESH;
+    // u32 MAX_SSTRESH;
+
+    // u32 MIN_CWND_CNT;
+    // u32 MAX_CWND_CNT;       
+
+    // u32 BASE_RTT;
+    // u32 CURRENT_RTT;
+    // u32 diff;
+
+    // u32 MIN_CWND;
+    // u32 MAX_CWND;
+
+    // TODO - Initialize min/max values from command line
+
+    tcp_vegas_register();
+
+///////// TCP initilization ///////////////////////
+	struct tcp_sock tcp_sock_struct; 
+	struct sock *sk = (struct sock *)&tcp_sock_struct;
+	struct tcp_sock *tp = &tcp_sock_struct;
+	struct inet_connection_sock *icsk = inet_csk(sk);
+    struct vegas *ca = inet_csk_ca(sk);
+
+    tcp_vegas_init(sk);
+
+    memset(&tcp_sock_struct, 0, sizeof(struct tcp_sock));
+	tcp_set_congestion_control(sk, &tcp_vegas); 
+	struct net net_namespace;
+	memset(&net_namespace, 0, sizeof(struct net));
+	tp->inet_conn.icsk_inet.sk.sk_net.net = &net_namespace;
+	tcp_sk_init(&net_namespace);
+	tcp_init_sock(sk);
+	if (icsk->icsk_ca_ops->init)
+		icsk->icsk_ca_ops->init(sk);
+	tcp_ca_event(sk, CA_EVENT_TX_START);
+
+///////// Symbolic Variable Initilization ///////////////////////
+    // u32 sym_ssthresh;       // tp
+    // u32 sym_max_cwnd;       // tp
+    // u32 sym_base_rtt;        // ca
+    // u32 sym_curr_rtt;       // ca 
+
+    tp->snd_cwnd = 10;
+    tp->snd_ssthresh = 1;
+    tp->snd_cwnd_cnt = 0;
+
+    
+
+    ///////// Verify Property /////////////////////////////
+	u32 cwnd_init = tp->snd_cwnd;
+    for(int i=0;i<cwnd_init;i++){
+
+        tcp_vegas_cong_avoid(sk,0,1);
+
+}
+
+    printf("snd_cwnd = %u, snd_ssthresh = %u, snd_cwnd_cnt = %u base_rtt = %u", tp->snd_cwnd,
+    tp->snd_ssthresh,tp->snd_cwnd_cnt, ca->baseRTT);
+
+    return 0;
+}
+
+#endif
